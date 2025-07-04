b63ec46698be63ecba0c3ae1158c70dbf9b38569
jtylab <1704657957@qq.com>
2025-07-04 09:18:14 +0800

    更改

---
+++ Task/Inc/CloudPlatform.h
@@ -0,0 +1,21 @@
+/**
+ * @file CloudPlatform.h
+ * @author jiang
+ * @brief 云台
+ * @version 0.1
+ * @date 2025-07-03
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+ #ifndef CloudPlatform_H
+ #define CloudPlatform_H
+
+ #include "main.h"
+
+
+
+
+ 
+ #endif // !1
---
+++ Task/Src/CloudPlatform.c
@@ -0,0 +1,10 @@
+/**
+ * @file CloudPlatform.c
+ * @author jiang
+ * @brief 云台
+ * @version 0.1
+ * @date 2025-07-03
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
